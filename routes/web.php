63c913542cb6bf16407dfe6cb5a3660cce03bb33
Sevtigo Pradono Wirawan <sevtigo@gmail.com>
2020-07-02 10:17:16 +0700

    buat route / dan /data-tables serta implementasi @stack dan @push

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/master', function () {
+    return view('adminlte.master');
+});
+
 Route::get('/', function () {
-    return view('master');
+    return view('items.index');
+});
+
+Route::get('/data-tables', function () {
+    return view('items.data_tables');
 });
